855bc35b4bf60651fdaf830da842f58daf5eecb6
GDLPratya <pratya.khaday@gmail.com>
2019-06-23 18:43:14 +0700

    init project

---
+++ src/main/java/com/tirmizee/SpringBootJPAHibernate/SpringBootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.tirmizee.SpringBootJPAHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaHibernateApplication.class, args);
+	}
+
+}
